13b18b830ca7cbf6ef167d4b54218a5f93bfdb6f
Scott Ashmore <sashmore@hi-res.net>
2016-10-29 12:06:35 +0100

    updates

--- keystone.js
+++ keystone.js
@@ -1,68 +1,36 @@
-/**
- * Simulate config options from your production environment by
- * customising the .env file in your project's root folder.
- */
 require('dotenv').config();
 
-/**
- * Require keystone
- */
-var keystone = require('keystone');
+const keystone = require('keystone');
 
-/**
- * Initialise Keystone with your project's configuration.
- * See http://keystonejs.com/guide/config for available options
- * and documentation.
- */
 keystone.init({
 	'name': 'Oscar Granse',
 	'brand': 'Oscar Granse',
-	
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'client/templates/views',
+	'static': 'dist',
+	'favicon': 'dist/favicon.ico',
+	'views': 'client',
 	'view engine': 'jade',
-	
 	'updates': 'server/updates',
 	'auto update': true,
 	'session': true,
 	'auth': true,
-	'user model': 'User'
+	'user model': 'User',
 });
 
-/**
- * Load your project's Models
- */
 keystone.import('./server/models');
 
-/**
- * Setup common locals for your templates. The following are required for the
- * bundled templates and layouts. Any runtime locals (that should be set uniquely
- * for each request) should be added to ./routes/middleware.js
- */
 keystone.set('locals', {
 	_: require('underscore'),
 	env: keystone.get('env'),
 	utils: keystone.utils,
-	editable: keystone.content.editable
+	editable: keystone.content.editable,
 });
 
-/**
- * Load your project's Routes
- */
 keystone.set('routes', require('./server/routes'));
 
-/**
- * Configure the navigation bar in Keystone's Admin UI
- */
 keystone.set('nav', {
-	'page': 'Page',
 	'posts': 'Post',
 	'tags' : 'Tags',
-	'users': 'User'
+	'users': 'User',
 });
 
-/**
- * Start Keystone to connect to your database and initialise the web server
- */
 keystone.start();
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-var keystone     = require('keystone');
-var middleware   = require('./middleware');
+var keystone = require('keystone');
+var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
 
 const api = importRoutes('../api')
@@ -8,19 +8,16 @@ const api = importRoutes('../api')
  * Setup Route Bindings
  */
 exports = module.exports = function(app) {
-  // API
   app.get('/api/post/:slug', keystone.middleware.api, api.posts.getPostById);
   app.get('/api/posts', keystone.middleware.api, api.posts.getAllPosts);
 	
-  // Views
 	app.get('/', function(req, res) {
     var view = new keystone.View(req, res);
     view.render('index');
   });
 
-  // Catch All
   app.get('*', function(req, res) {
     var view = new keystone.View(req, res);
-    view.render('errors/404');
+    view.render('404');
   });	
 };
