80b2c6a47e20b69d67375ed2ed3cc2c41285cac8
Scott Ashmore <dev.scottashmore@gmail.com>
2016-11-07 14:21:58 +0000

    using data

--- src/scripts/containers/Home.jsx
+++ src/scripts/containers/Home.jsx
@@ -5,6 +5,8 @@ import { increment, decrement } from 'actions/count';
 
 const mapStateToProps = (state) => ({
   count: state.count,
+  posts: state.posts,
+  tags: state.tags,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -21,6 +23,8 @@ const mapDispatchToProps = (dispatch) => ({
 export default class Home extends Component {
   static propTypes = {
     count: PropTypes.number.isRequired,
+    posts: PropTypes.array.isRequired,
+    tags: PropTypes.array.isRequired,
     onIncrement: PropTypes.func.isRequired,
     onDecrement: PropTypes.func.isRequired,
   };
@@ -41,13 +45,22 @@ export default class Home extends Component {
   }
 
   render() {
-    const { count } = this.props;
+    const { count, posts, tags } = this.props;
+    console.log(tags);
     return (
       <div className="Home">
         <h2>Home</h2>
         <span>Count: {count}</span>
         <button id="increment" onClick={this.buttonClicked}>INCREMENT</button>
         <button id="decrement" onClick={this.buttonClicked}>DECREMENT</button>
+        <h1>TAGS</h1>
+        <ul>{tags.map(tag => (
+          <li key={tag._id}>{tag.tag}</li>
+        ))}</ul>
+        <h1>POSTS</h1>
+        <ul>{posts.map(post => (
+          <li key={post._id}>{post.title}</li>
+        ))}</ul>
       </div>
     );
   }
